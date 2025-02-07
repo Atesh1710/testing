145950d44373d8147486ed4d27cbf12e9252d2c1
Pawangamaedge <pawan.dabi@gammaedge.io>
2025-02-07 12:54:40 +0530

    Added my db file

---
+++ db1.js
@@ -0,0 +1,27 @@
+const database = require('better-sqlite3')
+
+const db = new database('exp.db')
+
+if(db) console.log("db connected!")
+db.exec(`
+     CREATE TABLE IF NOT EXISTS users(
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     name TEXT NOT NULL,
+     city TEXT NOT NULL
+     )`
+)
+const saveUser = (name,city)=>{
+     const stmt = db.prepare(`INSERT INTO users(name,city) VALUES(?,?)`)
+     const info = stmt.run(name,city);
+     console.log(`data saved in user Table: ${info.lastInsertRowid}`);
+}
+
+const getUser = ()=>{
+     const stmt = db.prepare(`SELECT * FROM users where name LIKE 'p%'`)
+     const info = stmt.all()
+     console.log(`Show all records from user Table`,JSON.stringify(info,null,2));
+}
+
+saveUser("atesh","indore")
+saveUser("Piyush","TKG")
+getUser();
